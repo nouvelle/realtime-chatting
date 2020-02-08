74f0b905caadf05a83854e91d8fe639e7984ae5a
nouvelle <nouvellenuts@gmail.com>
2020-02-08 18:41:49 +0900

    add title/favicon

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -5,10 +5,10 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   entry: {
-    'index': [path.resolve('./src/javascripts/entry.js')]
+    index: [path.resolve('./src/javascripts/entry.js')],
   },
   output: {
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -16,20 +16,20 @@ module.exports = {
         enforce: 'pre',
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           postLoaders: {
-            js: 'babel-loader'
-          }
-        }
+            js: 'babel-loader',
+          },
+        },
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.pug$/,
@@ -37,18 +37,20 @@ module.exports = {
           // vueから呼ばれる場合
           {
             resourceQuery: /^\?vue/,
-            use: ['pug-plain-loader']
+            use: ['pug-plain-loader'],
           },
           // その他（htmlWebpackPluginからとか）
           {
-            use: [{
-              loader: 'pug-loader',
-              options: {
-                pretty: true
-              }
-            }]
-          }
-        ]
+            use: [
+              {
+                loader: 'pug-loader',
+                options: {
+                  pretty: true,
+                },
+              },
+            ],
+          },
+        ],
       },
       {
         test: /\.(jpg|png|gif|svg)$/,
@@ -58,10 +60,10 @@ module.exports = {
             options: {
               name: '[name].[ext]',
               outputPath: 'materials/',
-              publicPath: (url) => './materials/' + url
-            }
-          }
-        ]
+              publicPath: url => './materials/' + url,
+            },
+          },
+        ],
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
@@ -70,28 +72,28 @@ module.exports = {
           options: {
             name: '[name].[ext]',
             outputPath: 'fonts/',
-            publicPath: (url) => './fonts/' + url
-          }
-        }
-      }
-    ]
+            publicPath: url => './fonts/' + url,
+          },
+        },
+      },
+    ],
   },
   resolve: {
     extensions: ['.js', '.vue'],
     alias: {
-      vue$: 'vue/dist/vue.esm.js'
-    }
+      vue$: 'vue/dist/vue.esm.js',
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve('./src/html/index.pug'),
       hash: true,
-      inject: true
+      inject: true,
     }),
     new StyleLintPlugin({
       configFile: path.resolve(__dirname, '.stylelintrc.yml'),
-      files: ['**/*.scss', '**/*.vue']
+      files: ['**/*.scss', '**/*.vue'],
     }),
-    new VueLoaderPlugin()
-  ]
+    new VueLoaderPlugin(),
+  ],
 };
